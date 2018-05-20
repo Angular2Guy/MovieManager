3c6d2a6e5095f48b8bea93e3b1e3d7a97984e960
Angular2Guy <angular2guy@gmail.com>
2018-05-20 19:44:48 +0200

    make search case insensetive

--- src/main/java/ch/xxx/moviemanager/repository/CrudActorRepository.java
+++ src/main/java/ch/xxx/moviemanager/repository/CrudActorRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import ch.xxx.moviemanager.model.Actor;
 
 public interface CrudActorRepository extends JpaRepository<Actor,Long>{
-	@Query("select a from Actor a join a.users u where a.name like %:name% and u.id = :userid")
+	@Query("select a from Actor a join a.users u where lower(a.name) like lower(concat('%',:name,'%')) and u.id = :userid")
 	List<Actor> findByActorName(@Param("name") String name, @Param("userid") Long userId);
 }
--- src/main/java/ch/xxx/moviemanager/repository/CrudMovieRepository.java
+++ src/main/java/ch/xxx/moviemanager/repository/CrudMovieRepository.java
@@ -11,12 +11,12 @@ import ch.xxx.moviemanager.model.Movie;
 
 public interface CrudMovieRepository extends JpaRepository<Movie,Long>{
 
-	@Query("select e from Movie e join e.users u where e.title like %:title% and u.id = :userid")
+	@Query("select e from Movie e join e.users u where lower(e.title) like lower(concat('%',:title,'%')) and u.id = :userid")
 	List<Movie> findByTitle(@Param("title") String title, @Param("userid") Long userid);
 	
 	@Query("select m from Movie m join m.generes g join m.users u where g.genereId = :id and u.id = :userid")
 	List<Movie> findByGenereId(@Param("id") Integer id, @Param("userid") Long userid);
 	
-	@Query("select e from Movie e join e.users u where e.title like %:title% and e.releaseDate = :relDate and u.id = :userid")
+	@Query("select e from Movie e join e.users u where lower(e.title) like lower(concat('%',:title,'%')) and e.releaseDate = :relDate and u.id = :userid")
 	List<Movie> findByTitleAndRelDate(@Param("title") String title, @Param("relDate") Date releaseDate, @Param("userid") Long userid);
 }
