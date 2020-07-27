81f56fa51b36735098044a10c0d15d985338caab
Angular2Guy <angular2guy@gmail.com>
2020-07-27 22:51:18 +0200

    enable test

--- src/test/java/ch/xxx/moviemanager/MoviemanagerApplicationTests.java
+++ src/test/java/ch/xxx/moviemanager/MoviemanagerApplicationTests.java
@@ -1,10 +1,16 @@
 package ch.xxx.moviemanager;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MoviemanagerApplicationTests {
 
-	//@Test
+	@Test
 	public void contextLoads() {
 	}
 
