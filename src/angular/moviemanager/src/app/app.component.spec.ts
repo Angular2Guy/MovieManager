64294ab76620f329cdaaae34f8f38aaad9ea2d40
Angular2Guy <angular2guy@gmail.com>
2020-11-14 11:13:20 +0100

    angular 11 update

--- src/angular/moviemanager/src/app/app.component.spec.ts
+++ src/angular/moviemanager/src/app/app.component.spec.ts
@@ -10,11 +10,11 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -24,12 +24,12 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'app'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
