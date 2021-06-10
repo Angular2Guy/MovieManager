1f48ec521ed31afa48dad6f0eb9f8db859340c52
Angular2Guy <angular2guy@gmail.com>
2021-06-10 22:14:09 +0200

    updates

---
+++ src/main/java/ch/xxx/moviemanager/adapter/config/CronJobConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ *    Copyright 2019 Sven Loesekann
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package ch.xxx.moviemanager.adapter.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT3H")
+public class CronJobConfiguration {
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
+    }
+}
--- src/main/java/ch/xxx/moviemanager/adapter/cron/DatabaseCleanupJob.java
+++ src/main/java/ch/xxx/moviemanager/adapter/cron/DatabaseCleanupJob.java
@@ -10,11 +10,22 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-package ch.xxx.moviemanager.domain.model;
+package ch.xxx.moviemanager.adapter.cron;
 
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-public interface GenereRepository {	
-	public List<Genere> findAll();
-	public Genere save(Genere genere);
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+
+@Component
+public class DatabaseCleanupJob {
+	private static final Logger LOG = LoggerFactory.getLogger(DatabaseCleanupJob.class);
+
+	@Scheduled(cron = "5 0 * * * ?")
+	@SchedulerLock(name = "CleanUp_scheduledTask", lockAtLeastFor = "PT2H", lockAtMostFor = "PT3H")
+	public void dbCleanup() {
+		LOG.info("Cleanup Job");
+	}
 }
