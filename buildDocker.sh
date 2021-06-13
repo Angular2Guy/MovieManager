665391d15cebcd851802fd2f613d01bb57abf9c8
Angular2Guy <angular2guy@gmail.com>
2021-06-13 04:25:09 +0200

    update

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#./mvnw clean install -Dpostgresql=true -Dnpm.test.script=test-chromium
-./mvnw clean install -Dpostgresql=true
+#./mvnw clean install -Ddocker=true -Dnpm.test.script=test-chromium
+./mvnw clean install -Ddocker=true
 docker build -t angular2guy/moviemanager:latest --build-arg JAR_FILE=moviemanager-0.0.1-SNAPSHOT.jar --no-cache .
 docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/moviemanager:latest
