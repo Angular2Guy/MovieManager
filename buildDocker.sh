68437f4d2afb5cdc4e68528e868b52b3cf76d542
Angular2Guy <angular2guy@gmail.com>
2021-06-13 04:03:52 +0200

    update

---
+++ buildDocker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#./mvnw clean install -Dpostgresql=true -Dnpm.test.script=test-chromium
+./mvnw clean install -Dpostgresql=true
+docker build -t angular2guy/moviemanager:latest --build-arg JAR_FILE=moviemanager-0.0.1-SNAPSHOT.jar --no-cache .
+docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/moviemanager:latest
--- postgresql.sh
+++ postgresql.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker pull postgres
 docker run --name local-postgres -e POSTGRES_PASSWORD=sven1 -e POSTGRES_USER=sven1 -e POSTGRES_DB=movies -p 5432:5432 -d postgres
-# depricated psql -h localhost -U sven1 -f src/main/resources/db/initDB.sql movies
+# deprecated psql -h localhost -U sven1 -f src/main/resources/db/initDB.sql movies
 # docker start local-postgres
 # docker stop local-postgres
