331c43c121641ab00f073a17169c135499c64942
Angular2Guy <angular2guy@gmail.com>
2021-07-24 19:38:42 +0200

    fix

--- src/main/java/ch/xxx/moviemanager/domain/model/dto/MovieDto.java
+++ src/main/java/ch/xxx/moviemanager/domain/model/dto/MovieDto.java
@@ -23,11 +23,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class MovieDto {
 	private Long id;
 	private String overview;
-	@JsonProperty("releaseDate")
+	@JsonProperty("release_date")
 	private Date releaseDate;
 	private String title;	
 	private Long movieId;
-	@JsonProperty("genreIds")
+	@JsonProperty("genre_ids")
 	private int[] generes;
 	private List<CastDto> myCast = new ArrayList<>();
 	private List<GenereDto> myGenere = new ArrayList<>();
