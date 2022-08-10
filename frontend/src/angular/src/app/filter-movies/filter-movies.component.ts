dcf04d20769f852913776054926e76ef38a17c8c
Angular2Guy <angular2guy@gmail.com>
2022-08-10 11:27:21 +0200

    feat: add button

--- frontend/src/angular/src/app/filter-movies/filter-movies.component.ts
+++ frontend/src/angular/src/app/filter-movies/filter-movies.component.ts
@@ -11,6 +11,7 @@
    limitations under the License.
  */
 import { Component } from '@angular/core';
+import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { Movie } from '../model/movie';
 
 @Component({
@@ -21,8 +22,27 @@ import { Movie } from '../model/movie';
 export class FilterMoviesComponent {
   public filteredMovies: Movie[] = [];
   public filtering = false;
+  closeResult = '';
   
-  constructor() {}
+  constructor(private offcanvasService: NgbOffcanvas) {}
+
+  public open(content) {
+    this.offcanvasService.open(content, {ariaLabelledBy: 'offcanvas-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === OffcanvasDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on the backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   public selectMovie(movie: Movie): void {
 	
