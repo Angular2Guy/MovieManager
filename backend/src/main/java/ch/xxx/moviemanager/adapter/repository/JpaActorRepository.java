898a07577a53baa3a2a95fbb6bcb0023a5759826
angular2guy <angular2guy@gmail.com>
2022-11-15 12:55:38 +0100

    fix: update

--- backend/src/main/java/ch/xxx/moviemanager/adapter/repository/JpaActorRepository.java
+++ backend/src/main/java/ch/xxx/moviemanager/adapter/repository/JpaActorRepository.java
@@ -16,13 +16,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import ch.xxx.moviemanager.domain.model.entity.Actor;
 
-public interface JpaActorRepository extends PagingAndSortingRepository<Actor,Long>{
+public interface JpaActorRepository extends JpaRepository<Actor,Long>, PagingAndSortingRepository<Actor,Long>{
 	@Query("select a from Actor a join a.users u where lower(a.name) like lower(concat('%',:name,'%')) and u.id = :userid")
 	List<Actor> findByActorName(@Param("name") String name, @Param("userid") Long userId, Pageable pageable);
 	
