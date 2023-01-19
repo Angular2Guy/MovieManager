a3d6022d9a0767a86ae03a96b455a2830d4597df
angular2guy <angular2guy@gmail.com>
2023-01-19 08:14:34 +0100

    fix: clean up

--- backend/src/test/java/ch/xxx/moviemanager/adapter/controller/MovieControllerTest.java
+++ backend/src/test/java/ch/xxx/moviemanager/adapter/controller/MovieControllerTest.java
@@ -20,8 +20,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,8 +27,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import ch.xxx.moviemanager.domain.common.Role;
@@ -43,7 +41,9 @@ import ch.xxx.moviemanager.usecase.service.MovieService;
 @WebMvcTest(controllers = MovieController.class)
 @WithMockUser
 public class MovieControllerTest {
-	private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIiwiYXV0aCI6W3siYXV0aG9yaXR5IjoiVVNFUlMifV0sImxhc3Rtc2ciOjE2NzM5NzIyMjgxMTIsInV1aWQiOiI2N2ZhZjc1Mi0yMTUyLTQyYzMtODg4Ni1jMGQxMDY1M2Y4NDkiLCJpYXQiOjE2NzM5NzIyMjgsImV4cCI6MTY3Mzk3MjI4OH0.Y4yGhD3B5SH9swIsRrmT1vyssbGWnCOwZZLOLp7HKds";
+	private static final String TEST_SECRECT_KEY = "w1a7WlSUrqEfDvlVd47SGlwSb9CJk1BymiIhXXZP82MyNNxsY1krgpb0bQt-Z9uohLRR6afgBsRHP_qiaHQhb"
+			+ "wFNWJeTTWr1x28hABtuvbRGMdW9ihvM_8JpVDhwuFbr2YCUW_nBeqJwcT9h6024RB7gJRYdxy1R6-onq9VG-TAJ00lrsfpnWWWn7LSLoxkj4gxeLTaF_0hozjoZ"
+			+"90sTm3loeS0CfX2MgXi-UAdjsGG4ki40iw4wWrKverKUtZQPotcvObtTGdAEx4DfTGdU0ZK7O9IY9xxddoGxPgG9l2_ahhPjfqMJYPY-TuI_UXiKfbFhnRTrdg8GtXyU0G3GJQ==";
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -54,19 +54,21 @@ public class MovieControllerTest {
 	@MockBean
 	private DefaultMapper defaultMapper;
 
+	@SuppressWarnings("unchecked")
 	@BeforeEach
 	public void init() {
 		Mockito.when(this.defaultMapper.convertOnlyMovie(any(Movie.class))).thenCallRealMethod();
 		Mockito.when(this.defaultMapper.convert(any(Movie.class))).thenCallRealMethod();
-		Mockito.when(this.jwtTokenService.resolveToken(any(HttpServletRequest.class))).thenReturn("XXX");
-		Mockito.when(this.jwtTokenService.validateToken(any(String.class))).thenReturn(Boolean.TRUE);
-		Mockito.when(this.jwtTokenService.getAuthentication(any(String.class)))
-				.thenReturn(new UsernamePasswordAuthenticationToken("XXX", "", List.of(Role.USERS)));
+		Mockito.when(this.jwtTokenService.createToken(any(String.class), any(List.class), any(Optional.class))).thenCallRealMethod();
+		ReflectionTestUtils.setField(this.jwtTokenService, "secretKey", TEST_SECRECT_KEY);
+		Mockito.doCallRealMethod().when(this.jwtTokenService).init();
+		this.jwtTokenService.init();
 	}
 
 	@Test
 	public void movieSearchByNameTest() throws Exception {
-		final Movie myMovie = createTestMovieEntity();
+		final String TOKEN = this.jwtTokenService.createToken("XXX", List.of(Role.USERS), Optional.empty());
+		Movie myMovie = createTestMovieEntity();
 		Mockito.when(service.findMovie(any(String.class), any(String.class))).thenReturn(List.of(myMovie));
 		this.mockMvc
 				.perform(get("/rest/movie/xxx").header(JwtUtils.AUTHORIZATION, String.format("Bearer %s", TOKEN))
@@ -82,23 +84,25 @@ public class MovieControllerTest {
 		myMovie.setTitle("myTitle");
 		return myMovie;
 	}
-	
+
 	@Test
 	public void movieSearchByIdFoundTest() throws Exception {
-		final Movie myMovie = createTestMovieEntity();
+		final String TOKEN = this.jwtTokenService.createToken("XXX", List.of(Role.USERS), Optional.empty());
+		Movie myMovie = createTestMovieEntity();
 		Mockito.when(this.service.findMovieById(any(Long.class))).thenReturn(Optional.of(myMovie));
-		this.mockMvc.perform(get("/rest/movie/id/1").header(JwtUtils.AUTHORIZATION, String.format("Bearer %s", TOKEN))
+		this.mockMvc
+				.perform(get("/rest/movie/id/1").header(JwtUtils.AUTHORIZATION, String.format("Bearer %s", TOKEN))
 						.servletPath("/rest/movie"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.id", Matchers.is(Matchers.equalTo(myMovie.getId())), Long.class))
-		.andExpect(jsonPath("$.title", Matchers.is(Matchers.equalTo(myMovie.getTitle())), String.class));
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id", Matchers.is(Matchers.equalTo(myMovie.getId())), Long.class))
+				.andExpect(jsonPath("$.title", Matchers.is(Matchers.equalTo(myMovie.getTitle())), String.class));
 	}
-	
+
 	@Test
 	public void movieSearchByIdNotFoundTest() throws Exception {
+		final String TOKEN = this.jwtTokenService.createToken("XXX", List.of(Role.USERS), Optional.empty());
 		Mockito.when(this.service.findMovieById(any(Long.class))).thenReturn(Optional.empty());
 		this.mockMvc.perform(get("/rest/movie/id/1").header(JwtUtils.AUTHORIZATION, String.format("Bearer %s", TOKEN))
-						.servletPath("/rest/movie"))
-		.andExpect(status().isNotFound());
+				.servletPath("/rest/movie")).andExpect(status().isNotFound());
 	}
 }
