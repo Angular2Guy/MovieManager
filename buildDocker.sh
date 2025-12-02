3320e3257b0c8982fd8af266844276bdf898106f
Angular2guy <angular2guy@gmail.com>
2025-12-02 19:26:48 +0100

    fix: build

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #./mvnw clean install -Ddocker=true -Dnpm.test.script=test-chromium
 ./mvnw clean install -Ddocker=true
-docker build -t angular2guy/moviemanager:latest --build-arg JAR_FILE=moviemanager-backend-0.0.1-SNAPSHOT.jar --no-cache .
+docker build -t angular2guy/moviemanager:latest --no-cache .
 docker run -p 8080:8080 --memory="1g" --network="host" angular2guy/moviemanager:latest
